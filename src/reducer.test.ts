88094b3ad05a7f7cdaed55c06c74a008a9a62ba4
youxkei <youxkei@gmail.com>
2016-07-22 22:27:46 +0900

    Write timeTrackings reducer test

--- src/reducer.test.ts
+++ src/reducer.test.ts
@@ -193,3 +193,66 @@ describe('tasks reducer', function() {
         );
     });
 });
+
+describe('timeTrackings reducer', function() {
+    it('should reduce StartTimeTracking action', function() {
+        assert.deepEqual(
+            timeTrackings(
+                [],
+                { type: 'StartTimeTracking', taskId: 0, begin: new Date(314) },
+            ),
+            [
+                { id: 0, taskId: 0, begin: new Date(314), end: null },
+            ]
+        );
+
+        assert.deepEqual(
+            timeTrackings(
+                [
+                    { id: 0, taskId: 0, begin: new Date(314), end: null },
+                ],
+                { type: 'StartTimeTracking', taskId: 1, begin: new Date(141) },
+            ),
+            [
+                { id: 0, taskId: 0, begin: new Date(314), end: null },
+                { id: 1, taskId: 1, begin: new Date(141), end: null },
+            ]
+        );
+    });
+
+    it('should reduce StopTimeTracking action', function() {
+        assert.deepEqual(
+            timeTrackings(
+                [],
+                { type: 'StopTimeTracking', timeTrackingId: 3141592, end: new Date(314) }
+            ),
+            []
+        );
+
+        assert.deepEqual(
+            timeTrackings(
+                [
+                    { id: 0, taskId: 0, begin: new Date(141), end: null },
+                ],
+                { type: 'StopTimeTracking', timeTrackingId: 3141592, end: new Date(314) }
+            ),
+            [
+                { id: 0, taskId: 0, begin: new Date(141), end: null },
+            ]
+        );
+
+        assert.deepEqual(
+            timeTrackings(
+                [
+                    { id: 0, taskId: 1, begin: new Date(141), end: null },
+                    { id: 1, taskId: 0, begin: new Date(173), end: null },
+                ],
+                { type: 'StopTimeTracking', timeTrackingId: 1, end: new Date(314) }
+            ),
+            [
+                { id: 0, taskId: 1, begin: new Date(141), end: null },
+                { id: 1, taskId: 0, begin: new Date(173), end: new Date(314) },
+            ]
+        );
+    });
+});
