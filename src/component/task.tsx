493a7ff01679ef970079d00cf1ce43d19f04280e
youxkei <youxkei@gmail.com>
2016-07-23 21:19:18 +0900

    Implement Task component

---
+++ src/component/task.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Card, CardHeader, CardMedia } from 'material-ui/Card';
+
+export interface TaskProps {
+    title: string,
+    estimation: Date,
+    children: TaskProps[],
+}
+
+export function Task({ title, estimation, children }: TaskProps): React.ReactElement<any> {
+    return (
+        <Card>
+            <CardHeader title={title} />
+            <CardMedia>
+                { children.map(props => <Task {...props} />) }
+            </CardMedia>
+        </Card>
+    );
+}
+
+export default Task;
